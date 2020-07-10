3c9a80d60fe4e6932b9759c8ebfd6e341c3c72da
Daniel <daniel.c.derkacz@t-online.de>
2020-07-10 21:17:36 +0200

    some cmake testing

--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,12 @@
 #include <Kouky3d.h>
 
 int main (int argc, char *argv[]) { 
-    anotherTest();
+
+    Kouky3d::Test t = Kouky3d::Test();
+    t.basicFunction();
+
+    testPrint();
+
+    Kouky3d::basicFunctionCall();
     return 0;
 }
